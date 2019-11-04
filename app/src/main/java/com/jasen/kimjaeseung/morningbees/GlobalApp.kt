ea0c548f2b74a8321c13e865dc344657303b1e82
silver432 <pub60@hanmail.net>
2019-11-04 21:44:13 +0900

    first commit

---
+++ app/src/main/java/com/jasen/kimjaeseung/morningbees/GlobalApp.kt
@@ -0,0 +1,14 @@
+package com.jasen.kimjaeseung.morningbees
+
+import android.app.Application
+
+class GlobalApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
