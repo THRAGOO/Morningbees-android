57d9ba5b4e627783ef5af00f86dcbdf271fb3a18
kim jae seung <silvree@gmail.com>
2019-11-11 21:54:43 +0900

    get google naver access token

--- app/src/main/java/com/jasen/kimjaeseung/morningbees/login/LoginActivity.kt
+++ app/src/main/java/com/jasen/kimjaeseung/morningbees/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.jasen.kimjaeseung.morningbees.login
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -12,44 +13,142 @@ import com.google.android.gms.common.api.ApiException
 import com.jasen.kimjaeseung.morningbees.R
 import kotlinx.android.synthetic.main.activity_login.*
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLoginHandler
+import com.nhn.android.naverlogin.data.OAuthLoginState
+
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mGoogleSignInClient: GoogleSignInClient    //google sign in client
+    private lateinit var mOAuthLoginModule: OAuthLogin  //naver sign in module
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        initButtonListeners()
+
+        initGoogleSignIn()
+        initNaverSignIn()
+    }
+
+    private fun initButtonListeners(){
         // Button Listeners
         login_google_sign_in_button.setOnClickListener(this)
         login_google_sign_out_button.setOnClickListener(this)
+        login_naver_sign_in_button.setOnClickListener(this)
+    }
 
-        initGoogleSignIn()
+    private fun initNaverSignIn() {
+        mOAuthLoginModule = OAuthLogin.getInstance()
+        mOAuthLoginModule.init(
+            this, getString(R.string.naver_oauth_client_id)
+            , getString(R.string.naver_oauth_client_secret)
+            , getString(R.string.naver_oauth_client_name)
+        )
     }
 
     private fun initGoogleSignIn() {
-
         // configure Google Sign-in and the GoogleSignInClient object
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.google_server_client_id))
             .requestEmail()
             .build()
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        refreshIdToken()
+
         // check existing user
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-        if (account!=null) Log.d(TAG,"already ${account.displayName}")
+//        val account = GoogleSignIn.getLastSignedInAccount(this)
+//        if (account!=null) {
+//            Log.d(TAG,"already ${account.displayName}")
+//
+//        }
         //updateUI(acoount)
 
     }
 
-    private fun googleSignIn(){
+    private fun googleSignIn() {
         val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun googleSignOut(){
-        mGoogleSignInClient.signOut().addOnCompleteListener(this){
-            Log.d(TAG,"Sign Out")
+    private fun signOut() {
+        //google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+            Log.d(TAG, "Google Sign Out")
+        }
+
+        //naver sign out
+        mOAuthLoginModule.logout(this)
+
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val idToken = account!!.idToken
+
+            Log.d(TAG, "Id Token : " + idToken)
+
+            // TODO(developer): send ID Token to server and validate
+
+            //updateUI(account)
+        } catch (e: ApiException) {
+            Log.w(TAG, "handleSignInResult:error", e)
+            //updateUI(null)
+        }
+
+    }
+
+    private fun refreshIdToken() {
+        // Attempt to silently refresh the GoogleSignInAccount. If the GoogleSignInAccount
+        // already has a valid token this method may complete immediately.
+        //
+        // If the user has not previously signed in on this device or the sign-in has expired,
+        // this asynchronous branch will attempt to sign in the user silently and get a valid
+        // ID token. Cross-device single sign on will occur in this branch.
+        mGoogleSignInClient.silentSignIn()
+            .addOnCompleteListener(
+                this
+            ) { task -> handleSignInResult(task) }
+    }
+
+    private fun naverSignIn(){
+        if (mOAuthLoginModule.getState(this)==OAuthLoginState.OK){
+            Log.d(TAG,"Status don't need Naver Login")
+            //네이버 access token으로 앱 로그인
+        }else{
+            Log.d(TAG,"Status need login")
+            mOAuthLoginModule.startOauthLoginActivity(this,@SuppressLint("HandlerLeak")
+            object:OAuthLoginHandler(){
+                override fun run(success: Boolean) {
+                    if (success){
+                        val accessToken = mOAuthLoginModule.getAccessToken(this@LoginActivity)
+                        val refreshToken = mOAuthLoginModule.getRefreshToken(this@LoginActivity)
+                        val expiresAt = mOAuthLoginModule.getExpiresAt(this@LoginActivity)
+                        val tokenType = mOAuthLoginModule.getTokenType(this@LoginActivity)
+                        Log.i(TAG, "naver Login Access Token : $accessToken")
+                        Log.i(TAG, "naver Login refresh Token : $refreshToken")
+                        Log.i(TAG, "naver Login expiresAt : $expiresAt")
+                        Log.i(TAG, "naver Login Token Type : $tokenType")
+                        Log.i(TAG, "naver Login Module State : " +mOAuthLoginModule.getState(this@LoginActivity).toString())
+
+                        //액티비티 이동
+
+                    }else{
+                        val errorCode = mOAuthLoginModule.getLastErrorCode(this@LoginActivity).getCode()
+                        val errorDesc = mOAuthLoginModule.getLastErrorDesc(this@LoginActivity)
+                        Log.d(TAG,"errorCode:$errorCode, errorDesc:$errorDesc")
+
+                    }
+                }
+
+            })
         }
     }
 
@@ -57,29 +156,38 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         val i = v.id
         when (i) {
             R.id.login_google_sign_in_button -> googleSignIn()
-            R.id.login_google_sign_out_button -> googleSignOut()
+            R.id.login_google_sign_out_button -> signOut()
+            R.id.login_naver_sign_in_button -> naverSignIn()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        when (requestCode) {
+            // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+            RC_SIGN_IN -> {
+                // The Task returned from this call is always completed, no need to attach
+                // a listener.
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
 
-            try {
-                // Google Sign In was successful
-                val account = task.getResult(ApiException::class.java)
+                try {
+                    // Google Sign In was successful
+                    val account = task.getResult(ApiException::class.java)
 
-                Log.d(TAG,account!!.displayName)
+                    Log.d(TAG, account!!.displayName)
+
+                    handleSignInResult(task)
 
 
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e)
+                }
+            }
+            RC_GET_TOKEN -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleSignInResult(task)
             }
         }
     }
@@ -87,5 +195,6 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     companion object {
         private const val TAG = "LoginActivity"
         private const val RC_SIGN_IN = 9001
+        private const val RC_GET_TOKEN = 9002
     }
 }
