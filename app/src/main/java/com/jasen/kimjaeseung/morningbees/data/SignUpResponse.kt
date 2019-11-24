14f468b687debf04a547dd6e56ca0e4c44df49f7
kim jae seung <silvree@gmail.com>
2019-11-24 16:19:20 +0900

    add signin signup retrofit

---
+++ app/src/main/java/com/jasen/kimjaeseung/morningbees/data/SignUpResponse.kt
@@ -0,0 +1,6 @@
+package com.jasen.kimjaeseung.morningbees.data
+
+data class SignUpResponse (
+    var accessToken : String?=null,
+    var refreshToken : String?=null
+)
--- app/src/main/java/com/jasen/kimjaeseung/morningbees/signup/SignUpActivity.kt
+++ app/src/main/java/com/jasen/kimjaeseung/morningbees/signup/SignUpActivity.kt
@@ -1,72 +1,89 @@
 package com.jasen.kimjaeseung.morningbees.main
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.jasen.kimjaeseung.morningbees.MorningBeesService
+import android.util.Log
+import com.jasen.kimjaeseung.morningbees.network.MorningBeesService
 import com.jasen.kimjaeseung.morningbees.R
-import com.jasen.kimjaeseung.morningbees.ResponseDTO
-import kotlinx.android.synthetic.main.activity_signup.*
-import retrofit2.*
+import com.jasen.kimjaeseung.morningbees.data.NameValidataionCheckResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 /*Api 통신할 때 AsyncTask 말고 Retrofit library 이용*/
 
 class SignUpActivity : AppCompatActivity() {
-    var check : Boolean = false
+//    var check : Boolean = false
 
     val retrofit = Retrofit.Builder()
-        .addConverterFactory(
-            GsonConverterFactory.create())
-        .baseUrl("https://8ee441a8-3537-4ffa-837e-435fd5240a0a.mock.pstmn.io/api/v1/auth/sign_up")
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("https://f553233b-4d4c-4d45-bf1e-d3999008d933.mock.pstmn.io")
         .build()
 
-    val server = retrofit.create(MorningBeesService::class.java)
+    val service = retrofit.create(MorningBeesService::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-        attachButtonEvent()
+        nameCheckValidation()
+//        attachButtonEvent()
     }
 
-    private fun attachButtonEvent(){
-        val mNickname:String = signup_nickname_text.text.toString()
+    private fun nameCheckValidation() {
+        val tempName = "bees"
 
-        signup_nickname_check_button.setOnClickListener {
-            server.getRequest(/*mNickname*/"bees").enqueue(object: Callback<ResponseDTO> {
-                override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                }
-
-                override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
-                    println(response.body().toString())
-                    //data parsing - ex) this.nickname = nickname
-                    check = true
-                }
-            })
-        }
-
-        signup_start_button.setOnClickListener {
-            when {
-                !check -> {
-                    Toast.makeText(this@SignUpActivity, "중복체크를 확인해주세요", Toast.LENGTH_SHORT).show()
-                }
-                check -> {
-                    server.putRequest(/*mNickname*/"bees").enqueue(object: Callback<ResponseDTO>{
-                        override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
-                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                        }
-
-                        override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
-                            // saveNickname
-                        }
-                    })
-                    startActivity(Intent(this, MainActivity::class.java))
-                }
+        service.nameValidationCheck(tempName).enqueue(object : Callback<NameValidataionCheckResponse> {
+            override fun onFailure(call: Call<NameValidataionCheckResponse>, t: Throwable) {
+                Log.d(TAG,t.toString())
             }
-        }
+
+            override fun onResponse(call: Call<NameValidataionCheckResponse>, response: Response<NameValidataionCheckResponse>) {
+                Log.d(TAG,response.body().toString())
+            }
+        })
+
+    }
+
+    //    private fun attachButtonEvent(){
+//        val mNickname:String = signup_nickname_text.text.toString()
+//
+//        signup_nickname_check_button.setOnClickListener {
+//            server.getRequest(/*mNickname*/"bees").enqueue(object: Callback<ResponseDTO> {
+//                override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
+//                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//                }
+//
+//                override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
+//                    println(response.body().toString())
+//                    //data parsing - ex) this.nickname = nickname
+//                    check = true
+//                }
+//            })
+//        }
+//
+//        signup_start_button.setOnClickListener {
+//            when {
+//                !check -> {
+//                    Toast.makeText(this@SignUpActivity, "중복체크를 확인해주세요", Toast.LENGTH_SHORT).show()
+//                }
+//                check -> {
+//                    server.putRequest(/*mNickname*/"bees").enqueue(object: Callback<ResponseDTO>{
+//                        override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
+//                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//                        }
+//
+//                        override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
+//                            // saveNickname
+//                        }
+//                    })
+//                    startActivity(Intent(this, MainActivity::class.java))
+//                }
+//            }
+//        }
+//    }
+    companion object {
+        private const val TAG = "SignUpActivity"
     }
 }
