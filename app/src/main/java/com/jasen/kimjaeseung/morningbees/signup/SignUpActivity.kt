c4fc77d87173a97dc0b807fdc8a393ba47531495
gyurim <gyurim1250@gmail.com>
2019-12-23 14:52:51 +0900

    modify nickname input restriction

--- app/src/main/java/com/jasen/kimjaeseung/morningbees/signup/SignUpActivity.kt
+++ app/src/main/java/com/jasen/kimjaeseung/morningbees/signup/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.jasen.kimjaeseung.morningbees.main
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.provider.ContactsContract
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.jasen.kimjaeseung.morningbees.network.MorningBeesService
 import com.jasen.kimjaeseung.morningbees.R
@@ -18,6 +20,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.NumberFormat
+import java.util.regex.Pattern
 
 /*Api 통신할 때 AsyncTask 말고 Retrofit library 이용*/
 
@@ -40,10 +44,16 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun attachButtonEvent(){
         signup_nickname_check_button.setOnClickListener {
-            nameCheckValidation()
+            CloseKeyboard()
+            val checkName = nicknameFilter(signup_nickname_text.text)
+
+            if(checkName != ""){ // checkName: 조건 만족하는 닉네임 (만족 안 하면 null 들어감)
+                nameCheckValidation(checkName)
+            }
         }
 
         signup_start_button.setOnClickListener {
+            CloseKeyboard()
             when {
                 !check -> {
                     Toast.makeText(this@SignUpActivity, "중복체크를 확인해주세요", Toast.LENGTH_SHORT).show()
@@ -55,9 +65,17 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun nameCheckValidation() {
-        val tempName = signup_nickname_text.text.toString()
+    private fun nicknameFilter(source:CharSequence): String {
+        val ps: Pattern = Pattern.compile("^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\u318D\\u119E\\u11A2\\u2022\\u2025a\\u00B7\\uFE55]+$")
+        if(source == "" ||ps.matcher(source).matches()){
+            return source.toString()
+        }
+        Toast.makeText(this@SignUpActivity, "닉네임은 한글, 영어, 숫자 조합으로 가능합니다 (공백 불가)", Toast.LENGTH_SHORT).show()
 
+        return ""
+    }
+
+    private fun nameCheckValidation(tempName: String) {
         service.nameValidationCheck(tempName).enqueue(object : Callback<NameValidataionCheckResponse> {
             override fun onFailure(call: Call<NameValidataionCheckResponse>, t: Throwable) {
                 Log.d(TAG,t.toString())
@@ -72,6 +90,7 @@ class SignUpActivity : AppCompatActivity() {
 
                 check = true
                 signup_nickname_check_button.isEnabled = false
+
                 signup_nickname_text.addTextChangedListener(object: TextWatcher{
                     override fun afterTextChanged(s: Editable?) {}
 
@@ -113,6 +132,16 @@ class SignUpActivity : AppCompatActivity() {
                 }
         })
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun CloseKeyboard() {
+        var view = this.currentFocus
+
+        if(view != null)
+        {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     companion object {
